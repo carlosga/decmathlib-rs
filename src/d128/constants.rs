8804616c5957f75f4853ff9c0d780f0e52c6337d
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2024-01-17 19:42:24 +0100

    constant definitions

--- src/d128/constants.rs
+++ src/d128/constants.rs
@@ -10,6 +10,7 @@
 
 #![allow(unused)]
 #![allow(dead_code)]
+#![allow(non_upper_case_globals)]
 
 #[cfg(target_endian = "big")]
 pub (crate) const BID_HIGH_128W: usize = 0;
@@ -19,6 +20,54 @@ pub (crate) const BID_LOW_128W: usize = 1;
 pub (crate) const BID_HIGH_128W: usize = 1;
 #[cfg(target_endian = "little")]
 pub (crate) const BID_LOW_128W: usize = 0;
+
+pub (crate) const MASK_STEERING_BITS: u64         = 0x6000000000000000u64;
+pub (crate) const MASK_BINARY_EXPONENT1: u64      = 0x7fe0000000000000u64;
+pub (crate) const MASK_BINARY_SIG1: u64           = 0x001fffffffffffffu64;
+pub (crate) const MASK_BINARY_EXPONENT2: u64      = 0x1ff8000000000000u64; //used to take G[2:w+3] (sec 3.3)
+pub (crate) const MASK_BINARY_SIG2: u64           = 0x0007ffffffffffffu64; //used to mask out G4:T0 (sec 3.3)
+pub (crate) const MASK_BINARY_OR2: u64            = 0x0020000000000000u64; //used to prefix 8+G4 to T (sec 3.3)
+pub (crate) const UPPER_EXPON_LIMIT: u32          = 51;
+pub (crate) const MASK_EXP: u64                   = 0x7ffe000000000000u64;
+pub (crate) const MASK_EXP2: u64                  = 0x1fff800000000000u64;
+pub (crate) const MASK_SPECIAL: u64               = 0x7800000000000000u64;
+pub (crate) const MASK_NAN: u64                   = 0x7c00000000000000u64;
+pub (crate) const MASK_SNAN: u64                  = 0x7e00000000000000u64;
+pub (crate) const MASK_ANY_INF: u64               = 0x7c00000000000000u64;
+pub (crate) const MASK_INF: u64                   = 0x7800000000000000u64;
+pub (crate) const MASK_SIGN: u64                  = 0x8000000000000000u64;
+pub (crate) const MASK_COEFF: u64                 = 0x0001ffffffffffffu64;
+pub (crate) const BIN_EXP_BIAS: u64               = (0x1820u64 << 49);
+
+pub (crate) const EXP_MIN32: u32                  = 0x00000000;
+pub (crate) const EXP_MAX32: u32                  = 0x5f800000;
+pub (crate) const EXP_MIN: u64                    = 0x0000000000000000;     // EXP_MIN = (-6176 + 6176) << 49
+pub (crate) const EXP_MAX: u64                    = 0x5ffe000000000000u64;  // EXP_MAX = (6111 + 6176) << 49
+pub (crate) const EXP_MAX_P1: u64                 = 0x6000000000000000u64;  // EXP_MAX + 1 = (6111 + 6176 + 1) << 49
+pub (crate) const EXP_P1: u64                     = 0x0002000000000000u64;  // EXP_ P1= 1 << 49
+pub (crate) const expmin: i32                     = -6176; // min unbiased exponent
+pub (crate) const expmax: i32                     = 6111; // max unbiased exponent
+pub (crate) const expmin16: i32                   = -398; // min unbiased exponent
+pub (crate) const expmax16: i32                   = 369; // max unbiased exponent
+pub (crate) const expmin7: i32                    = -101; // min unbiased exponent
+pub (crate) const expmax7: i32                    = 90;  // max unbiased exponent
+
+pub (crate) const MASK_INF32: u32                  = 0x78000000;
+pub (crate) const MASK_ANY_INF32: u32              = 0x7c000000;
+pub (crate) const MASK_SIGN32: u32                 = 0x80000000;
+pub (crate) const MASK_NAN32: u32                  = 0x7c000000;
+pub (crate) const MASK_SNAN32: u32                 = 0x7e000000;
+pub (crate) const SIGNMASK32: u32                  = 0x80000000;
+pub (crate) const BID32_SIG_MAX: u32               = 0x0098967f;
+pub (crate) const BID64_SIG_MAX: u64               = 0x002386F26FC0ffffu64;
+pub (crate) const SIGNMASK64: u64                  = 0x8000000000000000u64;
+pub (crate) const MASK_STEERING_BITS32: u32        = 0x60000000;
+pub (crate) const MASK_BINARY_EXPONENT1_32: u32    = 0x7f800000;
+pub (crate) const MASK_BINARY_SIG1_32: u32         = 0x007fffff;
+pub (crate) const MASK_BINARY_EXPONENT2_32: u32    = 0x1fe00000; //used to take G[2:w+3] (sec 3.3)
+pub (crate) const MASK_BINARY_SIG2_32: u32         = 0x001fffff; //used to mask out G4:T0 (sec 3.3)
+pub (crate) const MASK_BINARY_OR2_32: u32          = 0x00800000;
+pub (crate) const MASK_SPECIAL32: u32              = 0x78000000;
 
 // TYPE parameters
 pub (crate) const BID128_MAXDIGITS: u32 = 34;
@@ -130,4 +179,3 @@ pub (crate) const SSNAN_MASK32: u32             = 0xfc000000;
 pub (crate) const QUIET_MASK32: u32             = 0xfdffffff;
 pub (crate) const MASK_BINARY_EXPONENT: u64     = 0x7ff0000000000000;
 pub (crate) const BINARY_EXPONENT_BIAS: u32     = 0x3ff;
-pub (crate) const UPPER_EXPON_LIMIT: u32        = 51;
