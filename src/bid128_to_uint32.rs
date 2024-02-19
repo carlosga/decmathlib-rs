9a956b80ab1c883a06d370f55d5497a9f57d33ce
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2024-02-19 20:03:47 +0100

    bid128_to_uint32_floor

--- src/bid128_to_uint32.rs
+++ src/bid128_to_uint32.rs
@@ -757,7 +757,7 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
     // check for NaN or Infinity
     if (x.w[1] & MASK_SPECIAL) == MASK_SPECIAL {
         // x is special
-        if (x.w[1] & MASK_NAN) == MASK_NAN {	// x is NAN
+        return if (x.w[1] & MASK_NAN) == MASK_NAN {	// x is NAN
             if (x.w[1] & MASK_SNAN) == MASK_SNAN {	// x is SNAN
                 // set invalid flag
                 *pfpsf |= StatusFlags::BID_INVALID_EXCEPTION;
@@ -769,7 +769,7 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
                 // return Integer Indefinite
                 res = 0x80000000;
             }
-            return res;
+            res
         } else {	// x is not a NaN, so it must be infinity
             if x_sign == 0 {	// x is +inf
                 // set invalid flag
@@ -782,7 +782,7 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
                 // return Integer Indefinite
                 res = 0x80000000;
             }
-            return res;
+            res
         }
     }
     // check for non-canonical values (after the check for special values)
@@ -900,10 +900,10 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
             // C1 = C1 + 1/2 * 10^ind where the result C1 fits in 127 bits
             tmp64 = C1.w[0];
             if ind <= 19 {
-                C1.w[0] = C1.w[0] + bid_midpoint64[(ind - 1) as usize];
+                C1.w[0] += bid_midpoint64[(ind - 1) as usize];
             } else {
-                C1.w[0] = C1.w[0] + bid_midpoint128[(ind - 20) as usize].w[0];
-                C1.w[1] = C1.w[1] + bid_midpoint128[(ind - 20) as usize].w[1];
+                C1.w[0] += bid_midpoint128[(ind - 20) as usize].w[0];
+                C1.w[1] += bid_midpoint128[(ind - 20) as usize].w[1];
             }
             if C1.w[0] < tmp64 {
                 C1.w[1] += 1;
@@ -1024,7 +1024,7 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
             }
             // general correction for RM
             if is_midpoint_lt_even || is_inexact_gt_midpoint {
-                Cstar.w[0] = Cstar.w[0] - 1;
+                Cstar.w[0] -= 1;
             } else {
                 // the result is already correct
             }
@@ -1040,7 +1040,7 @@ pub (crate) fn bid128_to_uint32_floor(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) 
         }
     }
 
-    return res;
+    res
 }
 
 /*
