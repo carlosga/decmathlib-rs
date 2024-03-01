edd11ba48831f27c39b62f54e681861b4261cd09
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2024-03-01 18:10:49 +0100

    · bid128_round_integral

--- src/bid128_round_integral.rs
+++ src/bid128_round_integral.rs
@@ -1543,17 +1543,17 @@ pub (crate) fn bid128_round_integral_positive(x: &BID_UINT128, pfpsf: &mut _IDEC
 /// floating-point value in the same format, using the rounding-to-zero mode;
 /// do not signal inexact exceptions
 pub (crate) fn bid128_round_integral_zero(x: &BID_UINT128, pfpsf: &mut _IDEC_flags) -> BID_UINT128 {
-    let mut res: BID_UINT128 = BID_UINT128::default();
+    let mut res: BID_UINT128 = Default::default();
     let x_sign: BID_UINT64;
     let x_exp: BID_UINT64;
     let exp: i32;			// unbiased exponent
     // Note: C1.w[1], C1.w[0] represent x_signif_hi, x_signif_lo (all are BID_UINT64)
-    let mut tmp1: BID_UI64DOUBLE = BID_UI64DOUBLE::default();
+    let mut tmp1: BID_UI64DOUBLE = Default::default();
     let x_nr_bits: usize;
     let mut q: i32;
     let ind: i32;
     let shift: i32;
-    let mut C1: BID_UINT128 = BID_UINT128::default();
+    let mut C1: BID_UINT128 = Default::default();
     // BID_UINT128 res is C* at first - represents up to 34 decimal digits ~ 113 bits
     let P256: BID_UINT256;
 
