ee5674404d13e41a817a9ebf7b08689508693b5e
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2025-03-02 16:52:30 +0100

    · Minor changes

--- src/bid128_rem.rs
+++ src/bid128_rem.rs
@@ -1,4 +1,4 @@
-/* ---------------------------------------------------------------------------------------------------*/
+/* -------------------------------------------------------------------------------------------------- */
 /* Port of the Intel Decimal Floating-Point Math Library decimal128 type to Rust.                     */
 /* decmathlib-rs - Copyright (C) 2023-2024 Carlos Guzmán Álvarez                                      */
 /* Licensed under the MIT license. See LICENSE file in the project root for full license information. */
